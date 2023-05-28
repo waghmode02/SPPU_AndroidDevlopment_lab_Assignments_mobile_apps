e2656e5c1ad2a84d904ed88f72e20ca417c18f6a
Nagesh Waghmode <126709953+waghmode02@users.noreply.github.com>
2023-05-28 16:33:00 +0530

    Add files via upload

---
+++ WebView_Exp10/app/src/main/java/com/example/webview_exp10/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.webview_exp10;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+    private ProgressBar mProgressBar;
+    private WebView mWebView;
+
+    private String urlToload="https://dypiemr.irins.org/profile/254490";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mProgressBar = findViewById(R.id.loading_progressbar);
+        mWebView = findViewById(R.id.webview);
+
+        mWebView.loadUrl(urlToload);
+
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.getSettings().setJavaScriptEnabled(true);
+
+        mWebView.setWebChromeClient(new WebChromeClient(){
+
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                mProgressBar.setProgress(newProgress);
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+                getSupportActionBar().setTitle(title);
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mWebView.canGoBack())
+        {
+            mWebView.goBack();
+        }else {
+            super.onBackPressed();
+        }
+    }
+}
